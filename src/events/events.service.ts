43ed184f2ea014eb4c112047a61c1a01dbf6a5fc
kim jong hwa <ehdxka3@naver.com>
2023-10-07 18:35:24 +0900

    [작업중] 이벤트 res 생성

---
+++ src/events/events.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class EventsService {
+  constructor(private prisma: PrismaService) {}
+  create(createEventDto: CreateEventDto) {
+    return 'This action adds a new event';
+  }
+
+  findAll() {
+    return `This action returns all events`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} event`;
+  }
+
+  update(id: number, updateEventDto: UpdateEventDto) {
+    return `This action updates a #${id} event`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} event`;
+  }
+}
