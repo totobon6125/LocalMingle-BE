b7110c56608c04fb81c5bcea85402860957aa482
erickimme <jaeyeonkim0919@gmail.com>
2023-10-10 16:25:08 +0900

    [작업완료] user basic CRUD, userDetail 작업 중

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
+// src/users/users.service.ts
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [PrismaModule],
 })
 export class UsersModule {}
