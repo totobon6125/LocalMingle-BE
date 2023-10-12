535a0cf64b3707c1c553122a7c1a8e5c14068931
HeeDragoN1123 <yhy7952@gmail.com>
2023-10-12 09:55:51 +0900

    [완료] 이메일 인증 (url 방식)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+//import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
