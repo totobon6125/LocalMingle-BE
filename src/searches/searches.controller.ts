daf6d594a3c8ffbe1b757a0b9cff6b8973614122
kim jong hwa <ehdxka3@naver.com>
2023-10-31 13:54:09 +0900

    [작업완료, 테스트 필요]: 검색 기능 수정

--- src/searches/searches.controller.ts
+++ src/searches/searches.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Injectable, Query } from '@nestjs/common';
 import { SearchesService } from './searches.service';
-import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { SearchesDto } from './searches.dto.ts/searches.dto';
 
 @Controller('search')
 @Injectable()
@@ -10,8 +11,9 @@ export class SearchesController {
 
   @Get()
   @ApiOperation({ summary: '이벤트네임 or 콘텐츠 검색' })
-  async searchByNameOrContent(@Query('query') query: string) {
-    const events = await this.searchesService.searchByNameOrContent(query);
+  async searchByLocation(@Query() searchesDto: SearchesDto) {
+    console.log(searchesDto.keyWord)
+    const events = await this.searchesService.search(searchesDto);
 
     const event = events.map((item) => {
       const { GuestEvents, HostEvents, ...rest } = item;
@@ -24,61 +26,4 @@ export class SearchesController {
       };
     });
     return event;
-  }
-
-  @Get('byLocation')
-  @ApiQuery({ name: 'doName', type: String, example: '서울특별시, 경기도 등등'})
-  async searchByLocation(@Query() query:any) {
-    const events = await this.searchesService.searchByLocation(query);
-
-    const event = events.map((item) => {
-      const { GuestEvents, HostEvents, ...rest } = item;
-      const hostUser = HostEvents[0].User.UserDetail;
-
-      return {
-        event: rest,
-        guestList: GuestEvents.length,
-        hostUser: hostUser,
-      };
-    });
-    return event;
-  }
-
-  @Get('byCategory')
-  @ApiOperation({ summary: '카테고리별 검색' })
-  @ApiQuery({ name: 'query', type: String, example:'☕맛집/커피, 🏃‍♂️운동/건강,🐾애완동물, 📕공부/교육' })
-  async searchByCategory(@Query('query') query: string) {
-    const events = await this.searchesService.searchByCategory(query);
-
-    const event = events.map((item) => {
-      const { GuestEvents, HostEvents, ...rest } = item;
-      const hostUser = HostEvents[0].User.UserDetail;
-
-      return {
-        event: rest,
-        guestList: GuestEvents.length,
-        hostUser: hostUser,
-      };
-    });
-    return event;
-  }
-
-  @Get('byVerify')
-  @ApiOperation({ summary: '🏡동네만 or 🙋‍♀️아무나 검색' })
-  @ApiQuery({ name: 'query', type: String, example: '🏡동네만, 🙋‍♀️아무나' })
-  async searchByVerify(@Query('query') query: string) {
-    const events = await this.searchesService.searchByVerify(query);
-
-    const event = events.map((item) => {
-      const { GuestEvents, HostEvents, ...rest } = item;
-      const hostUser = HostEvents[0].User.UserDetail;
-
-      return {
-        event: rest,
-        guestList: GuestEvents.length,
-        hostUser: hostUser,
-      };
-    });
-    return event;
-  }
-}
+  } }
--- src/searches/searches.service.ts
+++ src/searches/searches.service.ts
@@ -1,101 +1,50 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { SearchesDto } from './searches.dto.ts/searches.dto';
 
 @Injectable()
 export class SearchesService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async searchByNameOrContent(query: string) {
-    if (query.length < 2) {
-      throw new BadRequestException('검색어를 2글자 이상 입력해주세요')
-    }
-    const events = await this.prisma.event.findMany({
+  search(searchesDto: SearchesDto) {
+    return this.prisma.event.findMany({
       where: {
         isDeleted: false,
-        OR: [
-          { eventName: { contains: query } },
-          { content: { contains: query } },
+        AND: [
+          searchesDto.keyWord
+            ? {
+                OR: [
+                  { eventName: { contains: searchesDto.keyWord } },
+                  { content: { contains: searchesDto.keyWord } },
+                ],
+              }
+            : null,
+          {
+            isVerified:
+              searchesDto.verify == ''
+                ? { not: null }
+                : { contains: searchesDto.verify },
+          },
+          {
+            location_City:
+              searchesDto.city == ''
+                ? { not: null }
+                : { contains: searchesDto.city },
+          },
+          {
+            location_District:
+              searchesDto.guName == ''
+                ? { not: null }
+                : { contains: searchesDto.guName },
+          },
+          {
+            category:
+              searchesDto.category == ''
+                ? { not: null }
+                : { contains: searchesDto.category },
+          },
         ],
       },
-      include: {
-        HostEvents: {
-          select: {
-            User: {
-              select: {
-                UserDetail: true,
-              },
-            },
-          },
-        },
-        GuestEvents: true,
-        _count: {
-          select: {
-            Viewlogs: true,
-          },
-        },
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-    return events;
-  }
-
-  searchByLocation(query: any) {
-    return this.prisma.event.findMany({
-      where: { location_City: query.doName, isDeleted: false },
-      include: {
-        HostEvents: {
-          select: {
-            User: {
-              select: {
-                UserDetail: true,
-              },
-            },
-          },
-        },
-        GuestEvents: true,
-        _count: {
-          select: {
-            Viewlogs: true,
-          },
-        },
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-  }
-
-  searchByCategory(query:string) {
-    return this.prisma.event.findMany({
-      where: { category: query, isDeleted: false },
-      include: {
-        HostEvents: {
-          select: {
-            User: {
-              select: {
-                UserDetail: true,
-              },
-            },
-          },
-        },
-        GuestEvents: true,
-        _count: {
-          select: {
-            Viewlogs: true,
-          },
-        },
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-  }
-
-  searchByVerify(query:string) {
-    return this.prisma.event.findMany({
-      where: { isVerified: query, isDeleted: false },
       include: {
         HostEvents: {
           select: {
