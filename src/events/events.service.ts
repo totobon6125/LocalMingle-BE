b80321e471782f45d3150c673972e1cb0cfd1424
kim jong hwa <ehdxka3@naver.com>
2023-11-06 15:15:20 +0900

    [작업완료]: 캐싱 기능 적용

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,15 +1,21 @@
 import {
   BadRequestException,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class EventsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private prisma: PrismaService
+  ) {}
 
   // 1. 이벤트 생성
   async create(userId: number, createEventDto: CreateEventDto) {
@@ -36,32 +42,40 @@ export class EventsService {
   }
 
   // 2. 이벤트 전체 조회
-  findAll() {
-    return this.prisma.event.findMany({
-      where: {
-        isDeleted: false,
-      },
-      include: {
-        HostEvents: {
-          select: {
-            User: {
-              select: {
-                UserDetail: true,
+  async findAll() {
+    const cachedEvents:any = await this.cacheManager.get('events');
+    const cachedData = cachedEvents ? JSON.parse(cachedEvents) : null;
+    if (cachedData) {
+      return cachedData;
+    } else {
+      const events = await this.prisma.event.findMany({
+        where: {
+          isDeleted: false,
+        },
+        include: {
+          HostEvents: {
+            select: {
+              User: {
+                select: {
+                  UserDetail: true,
+                },
               },
             },
           },
-        },
-        GuestEvents: true,
-        _count: {
-          select: {
-            Viewlogs: true,
+          GuestEvents: true,
+          _count: {
+            select: {
+              Viewlogs: true,
+            },
           },
         },
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      await this.cacheManager.set('events', JSON.stringify(events));
+      return events;
+    }
   }
 
   // 3. 이벤트 상세 조회
